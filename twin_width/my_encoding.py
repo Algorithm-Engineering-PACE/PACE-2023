dcdf0aa39040518795991798b613cdb5d4f7713e
Yinon Horev <yinon.horev@gmail.com>
2023-05-27 15:49:53 +0300

    added davis's decoding functions

--- twin_width/my_encoding.py
+++ twin_width/my_encoding.py
@@ -24,6 +24,10 @@ class MyTwinWidthEncoding:
         self._parent_start_index = len(g.nodes) + 1
         self.child_end_index = 2 * len(g.nodes) - d - 3
 
+    def update_num_vertices(self, g, d):
+        self.num_total_vertices = (2 * len(g.nodes)) - d - 2
+        self.child_end_index = (2 * len(g.nodes)) - d - 3
+
     def remap_graph(self, g):
         self.node_map = {}
         cnt = 1
@@ -114,7 +118,7 @@ class MyTwinWidthEncoding:
     # and one right child
     def encode_parent_child(self):
         # every child has at most one parent
-        for c in range(1, self.child_end_index):
+        for c in range(1, self.child_end_index + 1):
             parent_left_list = [
                 self.left_child[p][c]
                 for p in range(self.max_parent_start_index(c), self.num_total_vertices + 1)
@@ -282,6 +286,7 @@ class MyTwinWidthEncoding:
                     self.formula.append([-self.left_child[i][j], -self.right_child[i][k]])
 
     def encode(self, g, d):
+        self.update_num_vertices(g, d)
         g = self.remap_graph(g)
         self.pool = IDPool()
         if self.card_enc != EncType.seqcounter:
@@ -300,142 +305,99 @@ class MyTwinWidthEncoding:
         return self.formula
 
     def run(self, g, solver, start_bound, verbose=True, check=True, timeout=0):
-        start = time.time()
-        cb = start_bound
+            start = time.time()
+            cb = start_bound
+            elim_order = None
 
-        if verbose:
-            print(f"Created encoding in {time.time() - start}")
+            if verbose:
+                print(f"Created encoding in {time.time() - start}")
 
-        done = []
-        c_slv = None
+            done = []
+            c_slv = None
 
-        def interrupt():
-            if c_slv is not None:
-                c_slv.interrupt()
-            done.append(True)
+            def interrupt():
+                if c_slv is not None:
+                    c_slv.interrupt()
+                done.append(True)
 
-        timer = None
-        if timeout > 0:
-            timer = Timer(timeout, interrupt)
-            timer.start()
+            timer = None
+            if timeout > 0:
+                timer = Timer(timeout, interrupt)
+                timer.start()
 
-        i = start_bound
-        while i > 0:
-            if done:
-                break
-            with solver() as slv:
-                c_slv = slv
-                formula = self.encode(g, i)
-                slv.append_formula(formula)
-
+            i = start_bound
+            while i > 0:
                 if done:
                     break
+                with solver() as slv:
+                    c_slv = slv
+                    formula = self.encode(g, i)
+                    slv.append_formula(formula)
 
-                if slv.solve() if timeout == 0 else slv.solve_limited():
-                    if verbose:
-                        print(f"Found {i}")
-                    # cb = self.decode(slv.get_model(), g, i)
-                    i = cb =  cb - 1
-                else:
-                    cb += 1
-                    if verbose:
-                        print(f"Failed {i}")
-                    break
-
-                if verbose:
-                    print(f"Finished cycle in {time.time() - start}")
-        if timer is not None:
-            timer.cancel()
-        if cb == 0: # note that twin-width 0 case is taken care of seperately in main
-            cb = 1
-        return cb, None, None, time.time() - start
-
-    def decode(self, model, g, d):
-        g = g.copy()
-        model = {abs(x): x > 0 for x in model}
-        unmap = {}
-        for u, v in self.node_map.items():
-            unmap[v] = u
-
-        # Find merge targets and elimination order
-        mg = {}
-        od = []
-
-        for i in range(1, len(g.nodes) + 1):
-            for j in range(1, len(g.nodes) + 1):
-                if model[self.ord[i][j]]:
-                    if len(od) >= i:
-                        print("Double order")
-                    od.append(j)
-            if len(od) < i:
-                print("Order missing")
-        if len(set(od)) < len(od):
-            print("Node twice in order")
-
-        for i in range(1, len(g.nodes) + 1 - d):
-            for j in range(i + 1, len(g.nodes) + 1):
-                if model[self.merge[i][j]]:
-                    if od[i - 1] in mg:
-                        print("Error, double merge!")
-                    mg[od[i - 1]] = od[j - 1]
-
-        # Check edges relation...
-        for i in range(0, len(g.nodes) - d):
-            for j in range(i + 1, len(g.nodes) - d):
-                if model[self.edge[i + 1][j + 1]] ^ g.has_edge(
-                    unmap[od[i]], unmap[od[j]]
-                ):
-                    if model[self.edge[i + 1][j + 1]]:
-                        print(
-                            f"Edge error: Unknown edge in model {i+1}, {j+1} = {od[i], od[j]}"
-                        )
+                    if slv.solve() if timeout == 0 else slv.solve_limited():
+                        model = slv.get_model()
+                        elim_order = self.elim_order(model, i) # TODO: find elim order only for the last d
+                        if verbose:
+                            print(f"Found {i}")
+                        i = cb =  cb - 1
                     else:
-                        print("Edge error: Edge not in model")
+                        cb += 1
+                        if verbose:
+                            print(f"Failed {i}")
+                        break
 
-        # Perform contractions, last node needs not be contracted...
-        for u, v in g.edges:
-            g[u][v]["red"] = False
+                    if verbose:
+                        print(f"Finished cycle in {time.time() - start}")
+            if timer is not None:
+                timer.cancel()
+            if cb == 0:  # note that twin-width 0 case is taken care of seperately in main
+                cb = 1
+            return cb, elim_order, None, time.time() - start
 
-        c_max = 0
-        step = 1
-        for n in od[:-d]:
-            t = unmap[mg[n]]
-            n = unmap[n]
-            tn = set(g.neighbors(t))
-            tn.discard(n)
-            nn = set(g.neighbors(n))
+    def elim_order(self, model, d):
+        model = {abs(x): x > 0 for x in model}
+        num_parents = self.num_total_vertices-self.num_original_vertices
+        node_id = [*range(self.num_original_vertices+1)]+[0]*(num_parents)
+        children_ids = [(0, 0)]*(self.num_total_vertices+2)
+        parent = [0]*(self.num_total_vertices+2)
 
-            for v in nn:
-                if v != t:
-                    # Red remains, should edge exist
-                    if v in tn and g[n][v]["red"]:
-                        g[t][v]["red"] = True
-                    # Add non-existing edges
-                    if v not in tn:
-                        g.add_edge(t, v, red=True)
-            for v in tn:
-                if v not in nn:
-                    g[t][v]["red"] = True
-            g.remove_node(n)
-
-            # Count reds...
-            for u in g.nodes:
-                cc = 0
-                for v in g.neighbors(u):
-                    if g[u][v]["red"]:
-                        cc += 1
-                        u2, v2 = (
-                            od.index(self.node_map[u]) + 1,
-                            od.index(self.node_map[v]) + 1,
-                        )
-                        u2, v2 = min(u2, v2), max(u2, v2)
-                        if not model[self.red[step][u2][v2]]:
-                            print(f"Missing red edge in step {step}")
-                if cc > d:
-                    print(f"Exceeded bound in step {step}")
-                c_max = max(c_max, cc)
-
-            step += 1
-        print(f"Done {c_max}/{d}")
-
-        return c_max
+        for i in range(self._parent_start_index, self.num_total_vertices+1):
+            left = right = 0
+            for j in range(1, i):
+                if model[self.left_child[i][j]]:
+                    if(left > 0):
+                        print(f"Two left childs for {i}")
+                        return None
+                    left = j
+                    if parent[j]:
+                        print(f"Two parents for {j} namely {parent[j]} and {i}")
+                        return None
+                    parent[j] = i
+                if model[self.right_child[i][j]]:
+                    if(right > 0):
+                        print(f"Two right childs for {i}")
+                        return None
+                    right = j
+                    if parent[j]:
+                        print(f"Two parents for {j} namely {parent[j]} and {i}")
+                        print(f"left child of {i} is {left} and right child of {i} is {right}")
+                        return None
+                    parent[j] = i
+            if left == 0:
+                print(f"no left child for {i} ")
+                return None
+            if right == 0:
+                print(f"no right child for {i} ")
+                return None
+            children_ids[i] = (node_id[left], node_id[right])
+            node_id[i] = node_id[left]
+        roots_ids = [node_id[i] for i in range(1, self.num_total_vertices+1) if not parent[i]]
+        if(len(roots_ids) != d+2):
+            print(f"Number of uncontracted vertices is {len(roots_ids)} but expected {d+2}")
+            print(f"parents:{parent}")
+            return None
+        root1_id = roots_ids[0]
+        final_contractions = []
+        for i in range(1, len(roots_ids)):
+            final_contractions.append((root1_id, roots_ids[i]))
+        return children_ids[self._parent_start_index:self.num_total_vertices+1] + final_contractions
